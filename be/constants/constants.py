ad33eec6f42d432fb38141ded1f46d8c95111de3
Daniel Bessonov <danielbessonov@Daniels-MBP-8.lan>
2023-11-02 09:45:54 -0400

    cors again

--- be/constants/constants.py
+++ be/constants/constants.py
@@ -1,6 +1,6 @@
 ORIGINS = [
     "http://localhost:3000",
     "http://0.0.0.0:3000",
-    "https://morpheus.pics",
-    "morpheus-landing.vercel.app",
+    "https://www.morpheus.pics",
+    "https://morpheus-landing.vercel.app",
 ]
