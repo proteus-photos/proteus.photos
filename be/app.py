3e5dd5b899a02a8ee2da793436ee2d8bdf2d856d
Daniel Bessonov <danielbessonov@Daniels-MBP-8.lan>
2023-11-02 10:27:31 -0400

    allow all origins

--- be/app.py
+++ be/app.py
@@ -15,7 +15,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
