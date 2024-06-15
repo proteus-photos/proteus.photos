bc1393afe0c145122c08378bc11f7f7dc2f62640
Divide-By-0 <aayushg@mit.edu>
2024-06-14 23:15:51 -0700

    fix name to proteus

--- fe/app/layout.tsx
+++ fe/app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Morpheus',
-  description: 'Play around with perceptual hashes!',
+  title: 'Proteus',
+  description: 'Proteus is an open-source platform for AI content provenance - leveraging proof of transformation to create incorruptible and robust watermarks. Play around with our perceptual hashes!',
 }
 
 export default function RootLayout({
