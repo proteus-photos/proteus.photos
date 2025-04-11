10a7e61de57c1e9222db746791d8c47c19b5f1da
Divide-By-0 <aayushg@mit.edu>
2025-04-11 08:04:38 -0700

    added github link

--- fe/app/page.tsx
+++ fe/app/page.tsx
@@ -32,6 +32,14 @@ const Home = () => {
             <a target="_blank" rel="noopener noreferrer" href="https://proteusphotos.notion.site/Proteus-V2-Transformation-Resistant-AI-Image-Watermarking-2fae4336b9534e6ab4410003461a2811" className="mb-4 text-xl bg-white-500 text-blue-500 border border-blue-500 py-2 px-6 rounded hover:bg-blue-800 w-full sm:w-auto mr-4">Read our blog post</a>
             <Link href="#perceptual-hashes-section" className="mb-4 text-xl border border-green-500 text-green-600 py-2 px-6 rounded hover:bg-green-100 w-full sm:w-auto mr-4">Try our playground</Link>
             <a target="_blank" rel="noopener noreferrer" href="https://youtu.be/b45hakfN1cM?t=9234" className="mb-4 text-xl border border-yellow-500 text-yellow-600 py-2 px-6 rounded hover:bg-yellow-100 w-full sm:w-auto mr-4">Watch 10 min talk</a>
+            <a target="_blank" rel="noopener noreferrer" href="https://github.com/proteus-photos" className="mb-4 text-xl border border-gray-500 text-gray-600 py-2 px-6 rounded hover:bg-gray-100 w-full sm:w-auto mr-4">
+              <span className="flex items-center">
+                <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" clipRule="evenodd" d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.46-1.16-1.11-1.47-1.11-1.47-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.87 1.52 2.34 1.07 2.91.83.09-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0012 2z"/>
+                </svg>
+                GitHub
+              </span>
+            </a>
           </div>
         </div>
       </div>
